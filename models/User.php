bd6d45ed8745789ff95747dc6cb9ea2343969710
Ian Agustin <inbagustin@gmail.com>
2020-04-17 14:19:16 +0800

    Primary Commit

---
+++ models/User.php
@@ -0,0 +1,51 @@
+<?php
+class User {
+    private $table = 'user-details';
+    private $conn;
+
+    private $id;
+    private $firstName;
+    private $lastName;
+
+    public function __construct(PDO $db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+        $query = 'SELECT * FROM ' . $this->table;
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function read_single() {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE id=:id';
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(':id', $this->id);
+
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->firstName = $result['firstName'];
+        $this->lastName = $result['lastName'];
+    }
+
+    public function create() {
+        $query = 'INSERT INTO ' . $this->table . ' firstName=:firstName, lastName=:lastName';
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(':firstName', $this->firstName);
+        $stmt->bindParam(':lastName', $this->lastName);
+
+        $stmt->execute();
+    }
+}
