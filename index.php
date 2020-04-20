8c112b1df07428475987b59b90dc98054ec8da59
Ian Agustin <inbagustin@gmail.com>
2020-04-20 17:11:39 +0800

    Bare bones config for Slim Framework Routing

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-// Entry for routing
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\OutputBufferingMiddleware;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath('/notes-api');
+
+$app->group('/notes', function (RouteCollectorProxy $group) {
+    $group->get('/', function ($request, $response, $args) {
+        
+    });
+});
+
+$app->group('/users', function (RouteCollectorProxy $group) {
+    $group->get('/', function ($request, $response, $args) {
+        
+    });
+});
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Welcome to the API');
+    return $response;
+});
+
+$app->run();
